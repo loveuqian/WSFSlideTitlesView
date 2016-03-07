d46328a7a49677af5c06d2b10c50deea5b2d99e0
WangShengFeng <loveuqian@qq.com>
2016-03-07 17:25:16 +0800

    创建项目

---
+++ WSFSlideTitlesView/WSFSlideTitlesView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSFSlideTitlesView
+//
+//  Created by WangShengFeng on 3/7/16.
+//  Copyright © 2016 WangShengFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
